33c9b5e21cea3bb60f4e0816b9c3269cad080957
guonaihong <guonaihong@qq.com>
2018-11-06 22:04:23 +0800

    完善test文件

--- ccqueue_test.cc
+++ ccqueue_test.cc
@@ -1,8 +1,28 @@
 #include "ccqueue.h"
 #include <string>
+#include <thread>
+#include <iostream>
 
 int main() {
     //Queue<std::string> q;
     Queue<std::string> q();
+    std::thread p([&]{
+            for (int i = 0; i < 1000; i++) {
+                std::string s = std::to_string(i);
+                q.push(s);
+            }
+    });
+
+    p.join();
+
+    std::thread c([&]{
+            for (;;) {
+                std::string s;
+                q.wait_and_pop(s);
+                std::cout << s << std::endl;
+            }
+    });
+
+    c.join();
     return 0;
 }
