d8aaa18d2c4403b01590f733f82f7cc03d22a70a
guonaihong <guonaihong@qq.com>
2018-11-08 22:38:35 +0800

    更新代码

--- ccfile_write.h
+++ ccfile_write.h
@@ -6,6 +6,14 @@
 
 using string = std::string;
 
+const string CCFILE_DEFAULT = "default";
+
+const int CCFILE_KB = 1024;
+const int CCFILE_MB = CCFILE_KB * 1024;
+const int CCFILE_GB = CCFILE_KB * 1024;
+
+const int CCFILE_DEFAULT_MAX_SIZE = 100 * CCFILE_MB;
+
 class CCFile {
 
     public:
@@ -14,33 +22,92 @@ class CCFile {
         int write(const char *buf, size_t count);
 
     private:
-        void   write_file_main();
         void   compression_main();
+        void   sort_and_del_main();
         int    max_size;
         int    max_archive;
+
         string dir;
         string prefix;
         
         int    fd;
 
-        std::thread write_file;
+        Queue<std::string>  qcompress;
+        Queue<bool>         qsort_and_del;
+
+        std::thread sort_and_del;
         std::thread compression;
 };
 
+string base(string path) {
+    if (path == "") {
+        return ".";
+    }
+
+    auto is_path_separator = [](char c)-> bool{
+        return c == '/';
+    };
+
+    while(path.size() > 0 && is_path_separator(path.c_str()[path.size()-1])) {
+        path.resize(path.size() - 1);
+    }
+
+    auto i = long(path.size() -1);
+
+    while(i >= 0 && !is_path_separator(path.c_str()[i])) {
+        i--;
+    }
+
+    if (i >= 0) {
+        return path.c_str() + i + 1;
+    }
+
+    if (path == "") {
+        return "/";
+    }
+
+    return "";
+}
+
 CCFile::CCFile(string prefix, string dir, int max_size, int max_archive) {
-    this->write_file = std::thread([this] {
-        this->write_file_main();
-    });
+    if (prefix == "") {
+        prefix = CCFILE_DEFAULT;
+    }
+
+    if (max_size == 0) {
+        max_size = CCFILE_DEFAULT_MAX_SIZE;
+    }
 
     this->compression = std::thread([this]{
         this->compression_main();
     });
-}
 
-void CCFile::write_file_main() {
+    this->sort_and_del = std::thread([this] {
+        this->sort_and_del_main();
+    });
+
+    if (dir.size() > 0) {
+
+        char last = dir.c_str()[dir.size()-1]; 
+        if(last != '/' && last != '\\') {
+        }
+    }
 }
 
 void CCFile::compression_main() {
+
+    for (;;) {
+    }
+}
+
+void CCFile::sort_and_del_main() {
+
+    for (;;) {
+
+        bool ok;
+        this->qsort_and_del.wait_and_pop(ok);
+
+    }
 }
 
 int CCFile::write(const char * buf, size_t count) {
